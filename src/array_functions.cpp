0ae6ca5dddc0ddc62f12c11dffcb9374fafd6b63
Kristian Crsoby <kristian.crosby.18@cnu.edu>
2020-02-18 22:30:54 -0500

    added functions

--- src/array_functions.cpp
+++ src/array_functions.cpp
@@ -1,19 +1,34 @@
 /*
  * functionstocomplete.cpp
  *
- *  Created on: Sep 10, 2017
- *      Author: keith
+ *  Created on: 02/18/20
+ *      Author: Kristian Crosby
  */
 
 //============================================================================
 //	TODO add necessary includes here
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <string.h>
+#include <sstream>
+#include <stdio.h>
+#include "array_functions.h"
+#include "constants.h"
+#include "utilities.h"
+#include <algorithm>
 //============================================================================
 
 //============================================================================
 //	stuff you will need
 //============================================================================
+using namespace std;
+using namespace constants;
 //TODO define a structure to track words and number of times they occur
-
+struct entry {
+	string word = "";
+	int num_word = 0;
+};
 //TODO add a global array of entry structs (global to this file)
 
 //TODO add variable to keep track of next available slot in array
